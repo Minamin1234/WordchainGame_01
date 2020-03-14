002aaaf9204f4bf5972a51eeb9449c481adea68b
Minamin <minamin37345@gmail.com>
2020-03-14 17:06:05 +0900

    Construction a function, Testing a code.
    Under construction.

--- WordChainGame_01/MainWindow.xaml.cs
+++ WordChainGame_01/MainWindow.xaml.cs
@@ -24,5 +24,86 @@ namespace WordChainGame_01
         {
             InitializeComponent();
         }
+
+
+
+        enum Open_Close
+        {
+            Open,
+            Close,
+        }
+        //Open_Close Current_Open_Close = Open_Close.Open;
+
+        enum UIs
+        {
+            None,
+            Start,
+            Game,
+        }
+
+        UIs Current_UI = UIs.Start;
+
+        Window Current_Window = null;
+
+
+
+
+        void Change_UI(UIs Change_To ,Open_Close Open_Close)
+        {
+            switch (Change_To)
+            {
+                case UIs.None:
+                    {
+                        break;
+                    }
+
+                case UIs.Start:
+                    {
+                        if(Current_UI != UIs.Start)
+                        {
+                            Window_Start UI_Start = new Window_Start();
+                            if (Current_UI != UIs.None)
+                            {
+                                UI_Start.Visibility = Visibility.Visible;
+                            }
+                            else
+                            {
+                                UI_Start.Show();
+                            }
+                            Current_UI = UIs.Start;
+                            Current_Window = UI_Start;
+                            
+
+                        }
+                        break;
+                    }
+
+                case UIs.Game:
+                    {
+                        if(Current_UI != UIs.Game)
+                        {
+                            
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    }
+
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        private void Clicked(object sender, RoutedEventArgs e)
+        {
+            Button Clicked_Button = (Button)sender;
+            Console.WriteLine(Clicked_Button.Name);
+            
+            
+        }
     }
 }
---
+++ WordChainGame_01/Window_Game.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WordChainGame_01
+{
+    /// <summary>
+    /// Window_Game.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window_Game : Window
+    {
+        public Window_Game()
+        {
+            InitializeComponent();
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
